564b9f52e99716c190ea50493677c8e4943cc4eb
Juho Keski-Rahkonen <juho.keski-rahkonen@maplet.com>
2019-04-19 18:15:09 +0300

    Add routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as Koa from 'koa';
-import * as mount from 'koa-mount';
-import * as graphqlHTTP from 'koa-graphql';
+import * as route from 'koa-route';
+import * as helmet from 'koa-helmet';
+import * as cors from '@koa/cors';
 
 import db from './database';
-import schema from './schema';
+import routes from './routes';
 
 interface KoaApp extends Koa {
   init(): Promise<void>;
@@ -11,19 +12,20 @@ interface KoaApp extends Koa {
 
 const app = new Koa() as KoaApp;
 
+app.use(cors());
+app.use(helmet());
+
+app.use(route.get('/breed', routes.list));
+app.use(route.get('/breed/search/:name', routes.searchByName));
+app.use(route.get('/breed/:id', routes.getById));
+
+const port = 4000;
+
 app.init = async function() : Promise<void> {
   this.context.db = await db();
-  while (!this.context.db) {
-    // Database failed to connect, retry in 10 seconds.
-    await (new Promise(resolve => setTimeout(resolve, 1000 * 10)));
-    this.context.db = await db();
-  }
 
-  this.use(mount('/graphql', graphqlHTTP({
-    schema
-  })));
-
-  this.listen(4000);
+  this.listen(port);
+  console.log("Server started at port", port);
 }
 
 app.init();
