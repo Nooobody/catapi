d3b43bf55defd797a1e7338aac6dbd7c906df5b3
Juho Keski-Rahkonen <juho.keski-rahkonen@maplet.com>
2019-04-20 16:13:20 +0300

    Setting up for production

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,16 @@
 
+import { Context } from '../node_modules/@types/koa/index';
+
 export default {
-  async list(ctx) {
+  async list(ctx: Context) {
     const breeds = await ctx.db.getAllBreeds();
     ctx.body = breeds;
   },
-  async searchByName(ctx, name) {
+  async searchByName(ctx: Context, name: string) {
     const breed = await ctx.db.searchBreedByName(name);
     ctx.body = breed;
   },
-  async getById(ctx, id) {
+  async getById(ctx: Context, id: string) {
     const breed = await ctx.db.getBreedById(id);
     ctx.body = breed;
   }
